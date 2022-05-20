7609d14374158d4360a200f05db03fd19465d70a
VacuunDecay <82185824+VacuunDecay@users.noreply.github.com>
2022-05-20 15:19:43 -0300

    Comentarios/Nomes de função
    Acicionei alguns comentarios
    e troquei o nome das funções por nomes que achei mais coerente

---
+++ 21/jogo.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void pontos(){
+    printf("Muitos pontos pra voce!!");
+}
--- 21/main.c
+++ 21/main.c
@@ -2,37 +2,52 @@
 #include <time.h>
 #include <stdlib.h>
 
-int baralho(int cartas[]){
-int mesa[52];
-int t,i;
-for( i=0;i<52;i++){
-    mesa[i] = (i/13+3)*100 + i%13 + 1;
-}
+int embaralhar(int cartas[]){
+	int mesa[52];
+	int t,i;
+	//inicializa um vetor com um baralho nao embaralhado
+	for( i=0;i<52;i++){
+		mesa[i] = (i/13+3)*100 + i%13 + 1;
+	}
+
+	//Embaralha o baralho e o guarda na variavel de destino
 	srand(time(NULL));
-	for (i = 0; i < 52; i++)
-	{
-		do
-		{
+	for (i = 0; i < 52; i++){
+		do{
 			t = rand() % 52;
 		} while (mesa[t] == 0);
+
 		cartas[i] = mesa[t];
 		mesa[t] = 0;
 	}
 
-return 0;
+	return 0;
 }
 
-int converter_naipe(int a){
-if ((a%100==11) ||(a%100==12) ||(a%100==13)) return (a/100)*100+10;
+//Converte o valor da carta para a sua pontuação
+int ponto_carta(int a){
+	if ((a%100==11) ||(a%100==12) ||(a%100==13)) return (a/100)*100+10;
 	else return a;
 
 }
 int jogar(void){
 
-return 0;
+	return 0;
 }
-int main(void)
-{
+int main(void){
 
+
+	//testes aleatorios
+	int baralho[52];
+	embaralhar(baralho);
+
+
+
+	printf("Ola mundo\n");
+	printf("Cartas do baralho: ");
+	for (int i = 0;i < 52; i++){
+		printf("%d, ", ponto_carta(baralho[i]%100));
+	}
+	
 	return 0;
 }
