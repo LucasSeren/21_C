5793b16672d07d225ce240d226547f83b6f92d42
LucasSeren <68658108+LucasSeren@users.noreply.github.com>
2022-05-20 09:37:08 -0300

    funcoes para embaralhar e converter naipes

--- 21/main.c
+++ 21/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
-int main()
+int baralho(int cartas[]){
+int mesa[52];
+int t,i;
+for( i=0;i<52;i++){
+    mesa[i] = (i/13+3)*100 + i%13 + 1;
+}
+	srand(time(NULL));
+	for (i = 0; i < 52; i++)
+	{
+		do
+		{
+			t = rand() % 52;
+		} while (mesa[t] == 0);
+		cartas[i] = mesa[t];
+		mesa[t] = 0;
+	}
+
+return 0;
+}
+
+int converter_naipe(int a){
+if ((a%100==11) ||(a%100==12) ||(a%100==13)) return (a/100)*100+10;
+	else return a;
+
+}
+int jogar(void){
+
+return 0;
+}
+int main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+
+	return 0;
 }
